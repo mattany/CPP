e86bf491cd49d163e713e6b3d99964b1bfcf6055
mattan <mattany@gmail.com>
2019-09-21 02:48:54 +0300

    final commit

--- cpp_ex3/SpamDetector.cpp
+++ cpp_ex3/SpamDetector.cpp
@@ -138,11 +138,11 @@ bool parseMessage(const std::vector<std::string> &keys,
     {
         return false;
     }
-    for (const std::pair<std::string, int> &pair : scoreMap)
+    while (getline(message, line))
     {
-        while (getline(message, line))
+        convertToLowercase(line);
+        for (const std::pair<std::string, int> &pair : scoreMap)
         {
-            convertToLowercase(line);
             size_t pos = line.find(pair.first, 0);
             while (pos != std::string::npos)
             {
@@ -150,7 +150,6 @@ bool parseMessage(const std::vector<std::string> &keys,
                 pos = line.find(pair.first, pos + pair.first.length());
             }
         }
-        message.clear();
     }
     message.close();
     return true;
